ccb3ea63b22f322c8273d8c4b9483027502ea8a6
martin <martin@hop.kcl.ac.uk>
2011-02-25 20:26:29 +0000

    train is in london

--- constants.lisp
+++ constants.lisp
@@ -1,7 +1,13 @@
 ("linux/videodev2.h")
 
 
-#.`((:integer s-fmt "VIDIOC_S_FMT")
+#.`((:integer ioc-s-fmt "VIDIOC_S_FMT")
+    (:integer ioc-dqbuf "VIDIOC_DQBUF")
+    (:integer ioc-qbuf "VIDIOC_QBUF")
+    (:integer ioc-streamoff "VIDIOC_STREAMOFF")
+    (:integer ioc-streamon "VIDIOC_STREAMON")
+    (:integer ioc-reqbufs "VIDIOC_REQBUFS")
+    (:integer ioc-querybuf "VIDIOC_QUERYBUF")
     ,@(loop for i in '("RGB32" "BGR32" "RGB24" "BGR24"
 		       "GREY" "YUYV" "UYVY" "YUV422P")
 	 collect
@@ -11,6 +17,13 @@
     (:type u8 "__u8")
     (:enum buf-type ((video-capture "V4L2_BUF_TYPE_VIDEO_CAPTURE")
 		     (video-output "V4L2_BUF_TYPE_VIDEO_OUTPUT")))
+    (:enum memory ((memory-mmap "V4L2_MEMORY_MMAP")
+		   (memory-user-pointer "V4L2_MEMORY_USERPTR")
+		   (memory-overlay "V4L2_MEMORY_OVERLAY")))
+    (:structure request-buffers ("struct v4l2_requestbuffers"
+				 (u32 count "__u32" "count")
+				 (buf-type type "enum v4l2_buf_type" "type")
+				 (memory memory "enum v4l2_memory" "memory")))
     (:structure buffer ("struct v4l2_buffer"
 			,@(loop for i in '("index" "bytesused" "flags" "sequence"
 					   "length") collect
@@ -22,4 +35,5 @@
 		      collect
 			`(u32 ,(intern (string-upcase i)) "__u32" ,i))))
     (:structure format ("struct v4l2_format"
-			(buf-type type "v4l2_buf_type" "type"))))
+			(buf-type type "v4l2_buf_type" "type")
+			(pix-format pix "v4l2_pix_format" "fmt.pix"))))
