7e80d3a2dbe0c49ed838ebbd5162f77af5386a6c
martin <martin@hop.kcl.ac.uk>
2011-03-07 23:04:59 +0000

    a few widgets, without function for now

--- video.lisp
+++ video.lisp
@@ -8,12 +8,35 @@
 
 #+nil
 (with-ltk ()
-  (let ((b (make-instance 'button 
-			  :master nil
-			  :text "Press me"
-			  :command (lambda ()
-				     (format t  "Hello")))))
-    (pack b)))
+  (let* ((bar (make-instance 'frame))
+	 (b (make-instance 'button 
+			   :master bar
+			   :text "Press me"
+			   :command (lambda ()
+				      (format t  "Hello"))))
+	 (combo (make-instance 'combobox :master bar :text "foo" :values '(foo bar baz)))
+	 (fscale (make-instance 'frame :master bar))
+	 (progress (make-instance 'progressbar :master fscale :value 0 :length 150))
+	 (scale (make-instance 'scale :master fscale :from 0 :to 100  :length 150 ))
+	 (separator (make-instance 'separator :master fscale))
+	 
+	 (fcheck (make-instance 'frame :master bar))
+	 (ch1 (make-instance 'check-button :master fcheck :text "Salt")))
+    (setf (value progress) 10)
+    (configure scale :orient :horizontal)
+    (bind *tk* "<Alt-q>" (lambda (event) (declare (ignore event)) (setf *exit-mainloop* t)))
+    (pack bar :side :bottom)
+    (pack b :side :top)
+    (pack combo)
+    (pack fscale)
+    (pack progress)
+    (pack scale)
+    (pack separator)
+    (pack fcheck)
+    (pack ch1)))
+
+#+nil
+(ltk::ltktest)
 
 (defmacro parse-capabilities (cap)
   (let* ((caps '(video-capture video-output video-overlay vbi-capture
